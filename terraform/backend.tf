2b53975f56de1078a7e767cb522a8360d7e6ff76
Leandro Soria <leandro.sorials88@gmail.com>
2022-10-10 22:23:11 -0300

    fix account_id into backend.tf file

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,7 +4,7 @@ terraform {
     region         = "us-east-1"
     encrypt        = true
     key            = "lambdatf/deploy.tfstate"
-    role_arn       = "arn:aws:iam::916514384804:role/Role_devsecops"
+    role_arn       = "arn:aws:iam::677825415277:role/Role_devsecops"
     #shared_credentials_file = "~/.aws/credentials"
   }
 }
